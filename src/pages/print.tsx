8e8f3f8f2fc593c3d26c2a2e206b0155881efacb
andrebrumdev <andre.brum@softlive>
2025-08-20 11:36:37 -0400

    Refactor InfracaoPagePrint component to include HTML structure and improve accessibility. Enhance layout with proper meta tags and ensure consistent styling for print output.

--- src/pages/print.tsx
+++ src/pages/print.tsx
@@ -82,210 +82,250 @@ export const InfracaoPagePrint = ({
   };
 }) => {
   return (
-    <table className="bg-white portrait-print ">
-      <thead className="border-b border-[#757E80]">
-        <tr>
-          <th colSpan={12} className="text-left text-xl text-[#757E80] py-2">
-            <div className="flex flex-row justify-between">
-              <span>
-                <img
-                  src="/images/logo-pdf.png"
-                  alt="logo SGTU"
-                  className="h-5"
-                />
-              </span>
-              <span>Auto de infração</span>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <Accordion
-              type="multiple"
-              className="space-y-4 py-4 px-0 "
-              value={Array.from({ length: 6 }, (_, i) => `item-${i + 1}`)}
-            >
-              {/* Número do Auto */}
-              <h1 className="text-xl text-[#757E80]">
-                Nº do Auto: {data?.id?.toString()?.padStart(6, "0")}
-              </h1>
-
-              {/* Identificação do Veículo */}
-              <AccordionItem value="item-1" className="space-y-5  border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Auto de Infração
+    <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      </head>
+      <body>
+        <table className="bg-white portrait-print ">
+          <thead className="border-b border-[#757E80]">
+            <tr>
+              <th
+                colSpan={12}
+                className="text-left text-xl text-[#757E80] py-2"
+              >
+                <div className="flex flex-row justify-between">
+                  <span>
+                    <img
+                      src="/images/logo-pdf.png"
+                      alt="logo SGTU"
+                      className="h-5"
+                    />
+                  </span>
+                  <span>Auto de infração</span>
                 </div>
-                <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-4">
-                  <FieldReadonly
-                    label="Modal"
-                    value={data?.vehicle?.modal?.name}
-                    classNameWrapper="!col-span-full"
-                  />
-                  <FieldReadonly
-                    label="Nº Ordem"
-                    value={data?.vehicle?.numOrder}
-                  />
-                  <FieldReadonly
-                    label="Nº Linha"
-                    value={data?.vehicle?.numLine}
-                  />
-                  <FieldReadonly
-                    label="Placa"
-                    value={data?.vehicle?.licensePlate}
-                  />
-                  <FieldReadonly label="Marca" value={data?.vehicle?.mark} />
-                  <FieldReadonly label="Modelo" value={data?.vehicle?.model} />
-                  <FieldReadonly
-                    label="Proprietário"
-                    value={data?.vehicle?.owner}
-                  />
-                </AccordionContent>
-              </AccordionItem>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Accordion
+                  type="multiple"
+                  className="space-y-4 py-4 px-0 "
+                  value={Array.from({ length: 6 }, (_, i) => `item-${i + 1}`)}
+                >
+                  {/* Número do Auto */}
+                  <h1 className="text-xl text-[#757E80]">
+                    Nº do Auto: {data?.id?.toString()?.padStart(6, "0")}
+                  </h1>
 
-              {/* Local e Data */}
-              <AccordionItem value="item-2" className="space-y-5 border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Identificação do Local da Infração
-                </div>
-                <AccordionContent className="grid grid-cols-12 gap-4 *:col-span-4">
-                  <FieldReadonly
-                    label="Data"
-                    value={new Date(data?.date ?? "").toLocaleDateString()}
-                  />
-                  <FieldReadonly label="Hora Inicial" value={data?.startTime} />
-                  <FieldReadonly label="Hora Final" value={data?.endTime} />
-                  <FieldReadonly
-                    label="Local da Infração"
-                    value={data?.location}
-                    classNameWrapper="!col-span-6"
-                  />
-
-                  <FieldReadonly
-                    label="Bairro"
-                    value={data?.neighborhood}
-                    classNameWrapper="!col-span-6"
-                  />
-                  <div className="!col-span-full flex flex-row gap-4 *:inline-flex *:gap-1 *:items-center">
-                    <span>
-                      <Checkbox
-                        checked={data?.direction === Direction.OUTBOUND}
-                      />
-                      Bairro / Centro
-                    </span>
-                    <span>
-                      <Checkbox
-                        checked={data?.direction === Direction.INBOUND}
-                      />
-                      Centro / Bairro
-                    </span>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-
-              {/* Assinatura */}
-              <AccordionItem value="item-3" className="space-y-5 border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Identificação da Infração
-                </div>
-                <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2  *:col-span-4">
-                  <FieldReadonly
-                    label="Lei Nº"
-                    value={data?.vehicle?.numLine}
-                  />
-                  <FieldReadonly
-                    label="Artigo"
-                    value={data?.vehicle?.numLine}
-                  />
-                  <FieldReadonly
-                    label="Inciso"
-                    value={data?.vehicle?.numLine}
-                  />
-                  <FieldReadonly
-                    label="Qtd. UFM"
-                    value={data?.vehicle?.numLine}
-                    classNameWrapper="!col-span-3"
-                  />
-
-                  <div className="!col-span-9 flex flex-row gap-4 items-end *:inline-flex *:gap-1 *:items-center">
-                    <span>Medida Adm.:</span>
-                    <span>
-                      <Checkbox
-                        checked={data?.direction === Direction.OUTBOUND}
-                      />
-                      Apreensão
-                    </span>
-                    <span>
-                      <Checkbox
-                        checked={data?.direction === Direction.INBOUND}
-                      />
-                      Retenção
-                    </span>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-
-              <AccordionItem value="item-4" className="space-y-5 border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Ciência e Prazo
-                </div>
-                <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-4">
-                  <p className="!col-span-full text-primary">
-                    Vossa Senhoria fica ciente de que, por força da Lei acima
-                    mencionada lhe é facultada a possibilidade de interpolação
-                    de recurso administrativo, no prazo de 15 (quinze) dias, a
-                    contar da data de ciência deste auto de infração.
-                  </p>
-                  <FieldReadonly
-                    label="Autuado"
-                    value={data?.vehicle?.owner}
-                    classNameWrapper="!col-span-full"
-                  />
-                  <div className="space-y-2 flex flex-col gap-1 !col-span-full">
-                    <div className="text-sm font-medium text-primary leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                      Assinatura do Autuado
+                  {/* Identificação do Veículo */}
+                  <AccordionItem
+                    value="item-1"
+                    className="space-y-5  border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Auto de Infração
                     </div>
-                    <div className="w-full border rounded-xl overflow-hidden h-40 bg-white">
-                      <img
-                        className="object-contain h-full w-full"
-                        src={data?.signature?.fileName}
-                        alt={data?.signature?.originalName}
+                    <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-4">
+                      <FieldReadonly
+                        label="Modal"
+                        value={data?.vehicle?.modal?.name}
+                        classNameWrapper="!col-span-full"
                       />
+                      <FieldReadonly
+                        label="Nº Ordem"
+                        value={data?.vehicle?.numOrder}
+                      />
+                      <FieldReadonly
+                        label="Nº Linha"
+                        value={data?.vehicle?.numLine}
+                      />
+                      <FieldReadonly
+                        label="Placa"
+                        value={data?.vehicle?.licensePlate}
+                      />
+                      <FieldReadonly
+                        label="Marca"
+                        value={data?.vehicle?.mark}
+                      />
+                      <FieldReadonly
+                        label="Modelo"
+                        value={data?.vehicle?.model}
+                      />
+                      <FieldReadonly
+                        label="Proprietário"
+                        value={data?.vehicle?.owner}
+                      />
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  {/* Local e Data */}
+                  <AccordionItem
+                    value="item-2"
+                    className="space-y-5 border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Identificação do Local da Infração
                     </div>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
+                    <AccordionContent className="grid grid-cols-12 gap-4 *:col-span-4">
+                      <FieldReadonly
+                        label="Data"
+                        value={new Date(data?.date ?? "").toLocaleDateString()}
+                      />
+                      <FieldReadonly
+                        label="Hora Inicial"
+                        value={data?.startTime}
+                      />
+                      <FieldReadonly label="Hora Final" value={data?.endTime} />
+                      <FieldReadonly
+                        label="Local da Infração"
+                        value={data?.location}
+                        classNameWrapper="!col-span-6"
+                      />
 
-              <AccordionItem value="item-5" className="space-y-5 border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Identificação do Fiscal
-                </div>
-                <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-6">
-                  <FieldReadonly label="Nome" value={data?.user?.name} />
-                  <FieldReadonly
-                    label="Matrícula"
-                    value={data?.user?.id.toString().padStart(4, "0")}
-                  />
-                </AccordionContent>
-              </AccordionItem>
+                      <FieldReadonly
+                        label="Bairro"
+                        value={data?.neighborhood}
+                        classNameWrapper="!col-span-6"
+                      />
+                      <div className="!col-span-full flex flex-row gap-4 *:inline-flex *:gap-1 *:items-center">
+                        <span>
+                          <Checkbox
+                            checked={data?.direction === Direction.OUTBOUND}
+                          />
+                          Bairro / Centro
+                        </span>
+                        <span>
+                          <Checkbox
+                            checked={data?.direction === Direction.INBOUND}
+                          />
+                          Centro / Bairro
+                        </span>
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
 
-              <AccordionItem value="item-6" className="space-y-5 border-none">
-                <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
-                  Observações
-                </div>
-                <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-full">
-                  <FieldReadonly
-                    label="Observações"
-                    value={data?.observation}
-                    as={Textarea}
-                  />
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+                  {/* Assinatura */}
+                  <AccordionItem
+                    value="item-3"
+                    className="space-y-5 border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Identificação da Infração
+                    </div>
+                    <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2  *:col-span-4">
+                      <FieldReadonly
+                        label="Lei Nº"
+                        value={data?.vehicle?.numLine}
+                      />
+                      <FieldReadonly
+                        label="Artigo"
+                        value={data?.vehicle?.numLine}
+                      />
+                      <FieldReadonly
+                        label="Inciso"
+                        value={data?.vehicle?.numLine}
+                      />
+                      <FieldReadonly
+                        label="Qtd. UFM"
+                        value={data?.vehicle?.numLine}
+                        classNameWrapper="!col-span-3"
+                      />
+
+                      <div className="!col-span-9 flex flex-row gap-4 items-end *:inline-flex *:gap-1 *:items-center">
+                        <span>Medida Adm.:</span>
+                        <span>
+                          <Checkbox
+                            checked={data?.direction === Direction.OUTBOUND}
+                          />
+                          Apreensão
+                        </span>
+                        <span>
+                          <Checkbox
+                            checked={data?.direction === Direction.INBOUND}
+                          />
+                          Retenção
+                        </span>
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem
+                    value="item-4"
+                    className="space-y-5 border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Ciência e Prazo
+                    </div>
+                    <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-4">
+                      <p className="!col-span-full text-primary">
+                        Vossa Senhoria fica ciente de que, por força da Lei
+                        acima mencionada lhe é facultada a possibilidade de
+                        interpolação de recurso administrativo, no prazo de 15
+                        (quinze) dias, a contar da data de ciência deste auto de
+                        infração.
+                      </p>
+                      <FieldReadonly
+                        label="Autuado"
+                        value={data?.vehicle?.owner}
+                        classNameWrapper="!col-span-full"
+                      />
+                      <div className="space-y-2 flex flex-col gap-1 !col-span-full">
+                        <div className="text-sm font-medium text-primary leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                          Assinatura do Autuado
+                        </div>
+                        <div className="w-full border rounded-xl overflow-hidden h-40 bg-white">
+                          <img
+                            className="object-contain h-full w-full"
+                            src={data?.signature?.fileName}
+                            alt={data?.signature?.originalName}
+                          />
+                        </div>
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem
+                    value="item-5"
+                    className="space-y-5 border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Identificação do Fiscal
+                    </div>
+                    <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-6">
+                      <FieldReadonly label="Nome" value={data?.user?.name} />
+                      <FieldReadonly
+                        label="Matrícula"
+                        value={data?.user?.id.toString().padStart(4, "0")}
+                      />
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem
+                    value="item-6"
+                    className="space-y-5 border-none"
+                  >
+                    <div className="border rounded text-primary p-2.5 border-[#C6D4D7] bg-[#F4F7F7]">
+                      Observações
+                    </div>
+                    <AccordionContent className="grid grid-cols-12 gap-y-4 gap-x-2 *:col-span-full">
+                      <FieldReadonly
+                        label="Observações"
+                        value={data?.observation}
+                        as={Textarea}
+                      />
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </body>
+    </html>
   );
 };
